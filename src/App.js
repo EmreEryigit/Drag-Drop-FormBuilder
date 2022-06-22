4e8645dea221bedc4380b2fe606bf753ba611ddf
Emre Eryiğit <emreeryigit@Emre-MacBook-Air.local>
2022-06-22 20:36:10 +0300

    completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ReactFormBuilder, ElementStore } from 'react-form-builder2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { get, post } from './requests';
+
+const getUrl = (cid) => `https://safe-springs-35306.herokuapp.com/api/formdata?cid=${cid}`;
+
+// const content = [
+//   {
+//     id: 'C68B673B-3948-4D62-AF6D-5320CAB4DDB7',
+//     element: 'TextInput',
+//     text: 'Text Input',
+//     required: true,
+//     canHaveAnswer: true,
+//     field_name: 'text_input_EEA6F5DA-5C2C-43D3-AB62-62385E3925D9',
+//     label: '<div>Name</div>\n',
+//   },
+//   {
+//     id: '6DAF1E95-44F6-4E5B-ABDD-D9A6BCA2C08A',
+//     element: 'TextInput',
+//     text: 'Text Input',
+//     required: true,
+//     canHaveAnswer: true,
+//     field_name: 'text_input_C5305462-9704-4E77-BFAB-A43C14AB2B8E',
+//     label: '<div>Email</div>\n',
+//   },
+// ];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { formId: '1' };
+    this.formId = this.state.formId;
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  formId;
+
+  handleChange(event) {
+    this.formId = event.target.value;
+    const url = getUrl(this.formId);
+    console.log('handleChange', url);
+    ElementStore.dispatch('load', { loadUrl: url });
+    this.setState({ formId: this.formId });
+  }
+
+  onLoad = () => {
+    const url = getUrl(this.formId);
+    console.log('onLoad', url);
+    return get(url);
+  };
+
+  onPost = (data) => {
+    const saveUrl = getUrl(this.formId);
+    console.log('onPost', saveUrl, data);
+    post(saveUrl, data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        
+        <ReactFormBuilder
+          // data={content}
+          onLoad={this.onLoad}
+          onPost={this.onPost}
+        />,
+      </div>
+    );
+  }
 }
 
 export default App;
+
